feb84f0e1f2f3f054495b67698863ea867c72969
unafil <229247848+unafil@users.noreply.github.com>
2025-11-24 15:34:56 -0800

    meta

--- Content.Shared/_Redshift/EntityEffects/Effects/ModifyArousal.cs
+++ Content.Shared/_Redshift/EntityEffects/Effects/ModifyArousal.cs
@@ -15,12 +15,12 @@ public sealed partial class ModifyArousal : EntityEffect
         if (!args.EntityManager.TryGetComponent(args.TargetEntity, out ArousalComponent? arousal))
             return;
 
-        if (MaxThreshold != null && arousal.CurrentArousal + Amount >= MaxThreshold)
+        if (MaxThreshold != null && Amount > 0 && arousal.CurrentArousal + Amount >= MaxThreshold)
             return;
 
         args.EntityManager.System<SharedArousalSystem>().ModifyArousal((args.TargetEntity, arousal), Amount);
     }
 
     protected override string? ReagentEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys)
-        => Loc.GetString("reagent-effect-guidebook-modify-arousal", ("chance", Probability), ("amount",  Amount), ("max", MaxThreshold == null? -1: MaxThreshold.Value));
+        => Loc.GetString("reagent-effect-guidebook-modify-arousal", ("chance", Probability), ("amount",  Amount), ("max", MaxThreshold == null? -1: MaxThreshold.Value), ("positive", Amount>0));
 }
--- Resources/Locale/en-US/_Redshift/chemistry/effects.ftl
+++ Resources/Locale/en-US/_Redshift/chemistry/effects.ftl
@@ -2,8 +2,11 @@ reagent-effect-guidebook-modify-arousal =
     { $chance ->
         [1] Modifies
         *[other] modify
-    } arousal by [color=pink]{NATURALFIXED($amount, 3)}[/color]%{ $max ->
-    
-        [-1],[color=pink] with no maximum limit[/color]
-        *[other], up to a maximum arousal of [color=pink]{NATURALFIXED($max, 3)}[/color]%
+    } arousal by [color=pink]{NATURALFIXED($amount, 3)}[/color]%{ $positive ->
+        [true]{ $max ->
+            
+            [-1],[color=pink] with no maximum limit[/color]
+            *[other], up to a maximum arousal of [color=pink]{NATURALFIXED($max, 3)}[/color]%
+        }
+        *[false] {""}
     }
